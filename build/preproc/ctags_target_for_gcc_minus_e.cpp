ece6fb3b61f9973471eb4547e6ef77b4f66b152e
Maciej Trudnos <m.trudnos@inseqr.pl>
2022-03-03 17:48:11 +0100

    Add project files

---
+++ build/preproc/ctags_target_for_gcc_minus_e.cpp
@@ -0,0 +1,10 @@
+# 1 "d:\\Projekty lokalne\\bodyfatiguedevice\\Program.ino"
+void setup()
+{
+
+}
+
+void loop()
+{
+
+}
---
+++ build/sketch/Program.ino.cpp
@@ -0,0 +1,17 @@
+#include <Arduino.h>
+#line 1 "d:\\Projekty lokalne\\bodyfatiguedevice\\Program.ino"
+#line 1 "d:\\Projekty lokalne\\bodyfatiguedevice\\Program.ino"
+void setup();
+#line 6 "d:\\Projekty lokalne\\bodyfatiguedevice\\Program.ino"
+void loop();
+#line 1 "d:\\Projekty lokalne\\bodyfatiguedevice\\Program.ino"
+void setup()
+{
+	
+}
+
+void loop()
+{
+	
+}
+
