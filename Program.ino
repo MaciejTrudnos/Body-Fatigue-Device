cc3bc8751ca897be5ffe56087ec5638a4d267972
Maciej Trudnos <m.trudnos@inseqr.pl>
2022-03-03 20:47:16 +0100

    Read data from heart rate sensor and send via bluetooth

--- Program.ino
+++ Program.ino
@@ -1,9 +1,24 @@
+#include <SoftwareSerial.h>
+SoftwareSerial BTserial(2, 3);
+
+char cstr[16];
+
 void setup()
 {
-	
+	Serial.begin(9600);
+    BTserial.begin(9600);
 }
 
 void loop()
 {
-	
+    int sensorValue = analogRead(A1);
+    
+    if(sensorValue) 
+    {
+        Serial.println(itoa(sensorValue, cstr, 10));
+        BTserial.write(itoa(sensorValue, cstr, 10));
+        BTserial.write('\n');
+    }
+    
+    delay(10);   
 }
--- build/sketch/Program.ino.cpp
+++ build/sketch/Program.ino.cpp
@@ -1,17 +1,32 @@
 #include <Arduino.h>
 #line 1 "d:\\Projekty lokalne\\bodyfatiguedevice\\Program.ino"
-#line 1 "d:\\Projekty lokalne\\bodyfatiguedevice\\Program.ino"
-void setup();
+#include <SoftwareSerial.h>
+SoftwareSerial BTserial(2, 3);
+
+char cstr[16];
+
 #line 6 "d:\\Projekty lokalne\\bodyfatiguedevice\\Program.ino"
+void setup();
+#line 12 "d:\\Projekty lokalne\\bodyfatiguedevice\\Program.ino"
 void loop();
-#line 1 "d:\\Projekty lokalne\\bodyfatiguedevice\\Program.ino"
+#line 6 "d:\\Projekty lokalne\\bodyfatiguedevice\\Program.ino"
 void setup()
 {
-	
+	Serial.begin(9600);
+    BTserial.begin(9600);
 }
 
 void loop()
 {
-	
+    int sensorValue = analogRead(A1);
+    
+    if(sensorValue) 
+    {
+        Serial.println(itoa(sensorValue, cstr, 10));
+        BTserial.write(itoa(sensorValue, cstr, 10));
+        BTserial.write('\n');
+    }
+    
+    delay(10);   
 }
 
